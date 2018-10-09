804340b2f9544d323a8befbf4bba16b8dbe106fd
sizumita <sumito@izumita.com>
2018-10-09 19:54:17 +0900

    円の半径

--- classes/math/Graph.py
+++ classes/math/Graph.py
@@ -14,9 +14,9 @@ def pie_chart(data: list, label: list, file_name: str):
     size = (50, 30)  # 凡例を配置する関係でsizeは横長にしておきます。
     col = cm.Spectral(np.arange(len(data)) / float(len(data)))
     plt.figure(figsize=size, dpi=100)
-    plt.pie(data, colors=col, counterclock=False, startangle=90, radius=0.01,
+    plt.pie(data, colors=col, counterclock=False, startangle=90,
             autopct=lambda p: '{:0.1f}%'.format(p) if p >= 5 else '')
-    plt.subplots_adjust(left=0, right=0.7)
+    plt.subplots_adjust(left=0, right=4)
     plt.legend(label, fancybox=True, loc='center left', bbox_to_anchor=(0.9, 0.5))
     plt.axis('equal')
     plt.savefig(f"{file_name}", bbox_inches='tight', pad_inches=0.05)
