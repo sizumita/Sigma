aaab492e43183269accb02f7eeaf39d6d64e2393
sizumita <sumito@izumita.com>
2018-10-09 17:32:06 +0900

    utf-8おテスト

--- classes/math/Graph.py
+++ classes/math/Graph.py
@@ -11,10 +11,10 @@ import numpy as np
 def pie_chart(data: list, label: list, file_name: str):
     plt.style.use('ggplot')
     plt.rcParams.update({'font.size': 15})
-    size = (9, 5)  # 凡例を配置する関係でsizeは横長にしておきます。
+    size = (20, 10)  # 凡例を配置する関係でsizeは横長にしておきます。
     col = cm.Spectral(np.arange(len(data)) / float(len(data)))
     plt.figure(figsize=size, dpi=100)
-    plt.pie(data, colors=col, counterclock=False, startangle=90,
+    plt.pie(data, colors=col, counterclock=False, startangle=90, radius=4,
             autopct=lambda p: '{:0.1f}%'.format(p) if p >= 5 else '')
     plt.subplots_adjust(left=0, right=0.7)
     plt.legend(label, fancybox=True, loc='center left', bbox_to_anchor=(0.9, 0.5))
