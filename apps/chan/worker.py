a755536ef1c6d7a1ba40c3c134c6a425d8f011b8
sizumita <sumito@izumita.com>
2018-10-17 13:26:51 +0900

    チャンネル追加

--- apps/chan/worker.py
+++ apps/chan/worker.py
@@ -261,7 +261,7 @@ class Worker(BaseWorker):
         await message.channel.send(content)
 
     async def on_message(self, message: discord.Message):
-        if message.channel.id in [501902669695418368, 501927723627970560, 501904504485183489]:
+        if message.channel.id in [501902669695418368, 501927723627970560, 501904504485183489, 501974208319062026]:
             self.client.loop.create_task(self.dialogue(message))
             return True
 
