088584d388f3e07d71c94cba9f855ac072522a19
sizumita <sumito@izumita.com>
2018-10-18 17:33:45 +0900

    機能改善

--- classes/TextGenerator/GenerateText.py
+++ classes/TextGenerator/GenerateText.py
@@ -85,7 +85,7 @@ class GenerateText(object):
         result = ""
         data = self.t.tokenize(content)
         try:
-            keys = [i.surface for i in data if [t for t in ["動詞", "名詞", "形容動詞", "形容詞"] if t in i.part_of_speech.split(",")][0]]
+            keys = [i.surface for i in data if [t for t in ["動詞", "名詞"] if t in i.part_of_speech.split(",")][0]]
         except IndexError:
             keys = [i.surface for i in data if not i in ["?", "？", "。", "、", "！", "!"]]
         for x in range(50):
